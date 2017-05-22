3bc1a575ebb5ec1a3afc4adfe79896f8cfa9f962
Javin Ambridge <javin.ambridge@gmail.com>
2017-05-22 14:29:40 -0700

    More refactoring

---
+++ src/client/app/shared/navigation/go-to-learn-more/go-to-learn-more.component.ts
@@ -0,0 +1,9 @@
+import { Component} from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'sd-go-to-learn-more',
+  templateUrl: 'go-to-learn-more.component.html',
+  styleUrls: ['go-to-learn-more.component.css', '../../../global-css/global.css'],
+})
+export class GoToLearnMoreComponent { }
--- src/client/app/shared/shared.module.ts
+++ src/client/app/shared/shared.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { BetViewComponent } from './bet-displaying/bet-view/bet-view.component';
 import { BetStateComponent } from './bet-displaying/bet-state/bet-state.component';
 import { ReturnHomeComponent } from './navigation/return-home/return-home.component';
+import { GoToLearnMoreComponent } from './navigation/go-to-learn-more/go-to-learn-more.component';
 
 /**
  * Do not specify providers for modules that might be imported by a lazy loaded module.
@@ -14,8 +15,8 @@ import { ReturnHomeComponent } from './navigation/return-home/return-home.compon
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [NavbarComponent, BetViewComponent, BetStateComponent, ReturnHomeComponent],
-  exports: [NavbarComponent, BetViewComponent, BetStateComponent, ReturnHomeComponent,
+  declarations: [NavbarComponent, BetViewComponent, BetStateComponent, ReturnHomeComponent, GoToLearnMoreComponent],
+  exports: [NavbarComponent, BetViewComponent, BetStateComponent, ReturnHomeComponent, GoToLearnMoreComponent,
     CommonModule, FormsModule, RouterModule]
 })
 export class SharedModule {
