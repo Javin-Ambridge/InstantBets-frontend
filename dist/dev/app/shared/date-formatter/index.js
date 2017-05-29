ea052ce7725cf1c68a1211f514a829f3e03cdd77
Jonah Glover <jonah@luxe.com>
2017-05-28 21:30:56 -0700

    fuck your dist

--- dist/dev/app/shared/date-formatter/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./date-formatter.service"));
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC9zaGFyZWQvZGF0ZS1mb3JtYXR0ZXIvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSw4Q0FBeUMiLCJmaWxlIjoiYXBwL3NoYXJlZC9kYXRlLWZvcm1hdHRlci9pbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCAqIGZyb20gJy4vZGF0ZS1mb3JtYXR0ZXIuc2VydmljZSc7Il19
