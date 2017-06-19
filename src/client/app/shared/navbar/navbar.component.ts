60da4bdd6114225fcf8b34ebdec79c6ce4c335aa
Javin-Ambridge <javin.ambridge@gmail.com>
2017-06-18 22:46:59 -0700

    Added user settings

--- src/client/app/shared/navbar/navbar.component.ts
+++ src/client/app/shared/navbar/navbar.component.ts
@@ -2,6 +2,8 @@ import { Component, ChangeDetectionStrategy, ChangeDetectorRef, Input, Output, E
 import { AuthService } from '../auth/auth.service';
 import { StateService } from '../state/state.service';
 import * as $ from 'jquery';
+import { AuthHttp } from 'angular2-jwt';
+import { Subject } from 'rxjs/Subject';
 
 /**
  * This class represents the navigation bar component.
@@ -18,15 +20,39 @@ export class NavbarComponent {
 	id: string = "nav-div";
 	loggedIn: boolean = false;
 	settingsOpen: boolean = false;
+	userInfo: any = undefined;
+	settings: any;
+	subs: any;
+	amountChange: Subject<string> = new Subject<string>();
+
 	@Output() logoutFunc = new EventEmitter<boolean>();
 
 	constructor(public auth: AuthService,
 		public stateService: StateService,
-    	public ref: ChangeDetectorRef) {
+    	public ref: ChangeDetectorRef,
+    	public authHttp: AuthHttp) {
 		stateService.state.subscribe((item) => {
 			this.loggedIn = item.loggedIn;
 			ref.markForCheck();
 		});
+        this.amountChange
+	        .debounceTime(1000)
+	        .switchMap(val => {
+	                console.log('called once: ' + val)
+	                return this.maxAmountUpdate(parseInt(val));
+	            }).subscribe(results => {
+	                console.log(results);
+	            }, error => {
+	                console.log('error logged:');
+	                console.log(error);
+	            });
+	}
+
+	maxAmountUpdate(val: number): any {
+		return this.authHttp.post(`http://localhost:3000/api/user-settings/max-amount`, {
+			val: JSON.stringify(val)
+		})
+	    .map(res => res.json());
 	}
 
 	attemptLogin(): void {
@@ -37,6 +63,16 @@ export class NavbarComponent {
 		this.logoutFunc.emit(true);
 	}
 
+	getUserInfo(): any {
+		this.authHttp.get(`http://localhost:3000/api/user-info`)
+	    .map(res => res.json())
+	    .subscribe((ret) => {
+	    	this.userInfo = ret.user_info;
+	    	this.settings = ret.settings;
+			this.ref.markForCheck();
+	    });
+	}
+
 	settingsClick(): void {
 		if (this.settingsOpen) {
 			$('#nav-div').css('clip-path', 'none');
@@ -44,14 +80,39 @@ export class NavbarComponent {
 				height: '48px'
 			}, "slow", () => {
 				this.settingsOpen = false;
+				this.ref.markForCheck();
 			});
 		} else {
 			$('#nav-div').animate({
-				height: '350px'
+				height: '450px'
 			}, "slow", () => {
 				$('#nav-div').css('clip-path', 'polygon(0 0, 100% 0, 100% 100%, 0% 90%)');
 				this.settingsOpen = true;
+				if (this.userInfo == undefined) {
+					this.getUserInfo();
+				}
+				this.ref.markForCheck();
 			});
 		}
+	}
+
+	emailNotifChange(newValue: any): void {
+		this.authHttp.post(`http://localhost:3000/api/user-settings/email`, {
+			val: JSON.stringify(newValue)
+		})
+	    .map(res => res.json())
+	    .subscribe((ret) => {});
+	}
+
+	maxBetAmountChange(newValue: any): void {
+		this.authHttp.post(`http://localhost:3000/api/user-settings/max-bet`, {
+			val: JSON.stringify(newValue)
+		})
+	    .map(res => res.json())
+	    .subscribe((ret) => {});
+	}
+
+	maxBetAmountValChange(newVal: string): void {
+		this.amountChange.next(newVal);
 	}
 }
